a3a5864f0a62a0a734591c7c76d7aa9e8ac4a497
desarrolloRM <desarrollorm@iMac-de-desarrolloRM.local>
2015-04-28 18:21:52 -0500

    Intentando agregar un tabla en la escena editarPerfil, que contiene el formulario para editar los datos del usuario (aun hace cosas raras)

---
+++ ControlEntradas/tFiPersonalizado.swift
@@ -0,0 +1,9 @@
+//
+//  tFiPersonalizado.swift
+//  ControlEntradas
+//
+//  Created by desarrolloRM on 28/04/15.
+//  Copyright (c) 2015 Desarrollo RM. All rights reserved.
+//
+
+import Foundation
---
+++ celda_Fecha.swift
@@ -0,0 +1,9 @@
+//
+//  celda_Fecha.swift
+//  ControlEntradas
+//
+//  Created by desarrolloRM on 28/04/15.
+//  Copyright (c) 2015 Desarrollo RM. All rights reserved.
+//
+
+import Foundation
